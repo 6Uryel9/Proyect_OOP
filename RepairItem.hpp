27599bb33454f48901444437ea556998f8f168ad
6Uryel9 <a01715507@tec.mx>
2026-06-14 15:31:03 -0600

    Add files via upload

---
+++ RepairItem.hpp
@@ -0,0 +1,21 @@
+#ifndef REPAIRITEM_HPP
+#define REPAIRITEM_HPP
+#include <string>
+#include <iostream>
+
+class RepairItem {
+private:
+    std::string partName;
+    double cost;
+    int quantity;
+public:
+    RepairItem(std::string name, double c, int q) : partName(name), cost(c), quantity(q) {}
+    double calculateSubtotal() const noexcept { return cost * quantity; }
+    
+    // Operator overloading for easy printing
+    friend std::ostream& operator<<(std::ostream& os, const RepairItem& item) {
+        os << item.quantity << "x " << item.partName << " @ $" << item.cost << " = $" << item.calculateSubtotal();
+        return os;
+    }
+};
+#endif
