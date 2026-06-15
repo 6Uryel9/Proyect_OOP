97a0523627f49098626baf8870c1d65cad2bd98d
6Uryel9 <a01715507@tec.mx>
2026-06-14 21:49:13 -0600

    Update Console.hpp

--- Console.hpp
+++ Console.hpp
@@ -9,10 +9,7 @@ protected:
 public:
     Console(std::string serial, std::string maker) : serialNumber(serial), manufacturer(maker) {}
     
-    // Virtual destructor guarantees correct destruction chain
     virtual ~Console() = default;
-    
-    // Pure virtual method makes this class Abstract
     virtual std::string diagnoseHardware() const = 0;
 };
 #endif
