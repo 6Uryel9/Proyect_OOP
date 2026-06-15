016e1f58f3e2fbaadc004ed6f75ac7125d91c26c
6Uryel9 <a01715507@tec.mx>
2026-06-14 21:45:47 -0600

    Update RepairItem.hpp

--- RepairItem.hpp
+++ RepairItem.hpp
@@ -12,7 +12,6 @@ public:
     RepairItem(std::string name, double c, int q) : partName(name), cost(c), quantity(q) {}
     double calculateSubtotal() const noexcept { return cost * quantity; }
     
-    // Operator overloading for easy printing
     friend std::ostream& operator<<(std::ostream& os, const RepairItem& item) {
         os << item.quantity << "x " << item.partName << " @ $" << item.cost << " = $" << item.calculateSubtotal();
         return os;
